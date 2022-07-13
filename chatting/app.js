f99401a8ff51043d9474d18e4008d630a59a0ab8
sol <soool1021@gmail.com>
2022-07-13 16:59:20 +0900

    메세지 전송

--- chatting/app.js
+++ chatting/app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 io.on("connection", (socket) => {
   socket.on("chatting", (data) => {
     console.log(data);
-    io.emit("chatting", `그래 반가워 ${data}`)
+    io.emit("chatting", data);
   });
 });
 
--- chatting/src/js/chat.js
+++ chatting/src/js/chat.js
@@ -2,9 +2,23 @@
 
 const socket = io();
 
-socket.emit("chatting", "from front");
+const nickname = document.querySelector("#nickname");
+const chatList = document.querySelector(".chatting-list");
+const chatInput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector(".send-button");
+
+sendButton.addEventListener("click", () => {
+  const param = {
+    name: nickname.value,
+    msg: chatInput.value,
+  };
+  socket.emit("chatting", param);
+});
+
 socket.on("chatting", (data) => {
-  console.log(data);
+  const li = document.createElement("li");
+  li.innerText = `${data.name}님이 - ${data.msg}`;
+  chatList.appendChild(li);
 });
 
 console.log(socket);
