5f024eb349d28d433191e1a1f20f2a7807ca7aad
sol <soool1021@gmail.com>
2022-07-13 19:41:04 +0900

    chat

--- chatting/app.js
+++ chatting/app.js
@@ -5,14 +5,19 @@ const path = require("path");
 const server = http.createServer(app);
 const socketIO = require("socket.io");
 const io = socketIO(server);
+const moment = require("moment");
 
 app.use(express.static(path.join(__dirname, "src")));
 const PORT = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
   socket.on("chatting", (data) => {
-    console.log(data);
-    io.emit("chatting", data);
+    const { name, msg } = data;
+    io.emit("chatting", {
+      name: name,
+      msg: msg,
+      time: moment(new Date()).format("h:mm A"),
+    });
   });
 });
 
